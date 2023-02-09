7d5800fdcb6bab963a60e14618c9a942418ee790
yisus82 <djalma8@gmail.com>
2023-02-09 13:51:31 +0100

    Communities actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :accounts
+
+  resources :communities
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -31,12 +31,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_08_120228) do
   end
 
   create_table "communities", force: :cascade do |t|
+    t.bigint "account_id"
     t.string "name"
     t.string "url"
     t.text "rules"
     t.integer "total_members"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_communities_on_account_id"
   end
 
 end
