237d2ab0f8eccce9cac58465bd7e85f2440ebd76
yisus82 <djalma8@gmail.com>
2023-02-14 12:14:42 +0100

    Account settings

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,4 +5,14 @@ class Account < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :communities
   has_many :posts
+
+  def full_name
+    if first_name.blank?
+      "#{last_name}"
+    elsif last_name.blank?
+      "#{first_name}"
+    else
+      "#{first_name} #{last_name}"
+    end
+  end
 end
